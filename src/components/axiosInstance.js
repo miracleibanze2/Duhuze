fdd7744ec7c18e1b198acbe3d0540a91f5ea0a76
miracleibanze2 <miracleibanze@gmail.com>
2024-12-26 19:47:30 +0200

    added .env

--- src/components/axiosInstance.js
+++ src/components/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const origin = import.meta.env.VITE_API_URL;
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: origin,
   headers: {
     "Content-Type": "application/json",
   },
